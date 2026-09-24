d825859bc73315b0865eaed62b6713f0b58b8a26
cherchezvous-uyen <142600133+cherchezvous-uyen@users.noreply.github.com>
2026-09-24 09:48:44 +0000

    scoring added

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let humanScore = 0;
 let computerScore = 0;
 let round = 0;
+const resultText = document.querySelector("#result-text");
+const resultScore = document.querySelector("#result-score");
 function computerPlay() {
     let random = Math.floor(Math.random()*3);
     if(random == 0) {
@@ -12,12 +14,31 @@ function computerPlay() {
         return "scissors"
     }
 }
-function playRound()
-const buttons = document.querySelectorAll('.button')
+function playRound(playerChoice, computerChoice) {
+    if ((playerChoice == 'paper' && computerChoice == 'rock') || 
+    (playerChoice == 'scissors' && computerChoice == 'paper') || 
+    (playerChoice == 'rock' && computerChoice == 'scissors') ){
+        humanScore++;
+        return "Player wins!";
+    }
+    else {
+        computerScore++;
+        return "Computer wins!";
+    }
+}
+const buttons = document.querySelectorAll('.choice')
 buttons.forEach(function(button){
     button.addEventListener('click', function(){
-        const playerChoice = button.data.choice;
+        const playerChoice = button.dataset.choice;
         const computerChoice = computerPlay();
-        playRou
+        const result = playRound(playerChoice, computerChoice);
+        
+        resultText.textContent = result;
+        resultScore.textContent = `${humanScore} - ${computerScore}`;
+
+        console.log(playerChoice);
+        console.log(computerChoice);
+        console.log(result);
+
     })
 })
