672f3127d7d86a07b30e137cadc113f271baab3a
cherchezvous-uyen <142600133+cherchezvous-uyen@users.noreply.github.com>
2026-09-25 04:53:30 +0000

    get play  round

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let computerScore = 0;
 let round = 0;
 const resultText = document.querySelector("#result-text");
 const resultScore = document.querySelector("#result-score");
+const roundText = document.querySelector("#round-number");
 function computerPlay() {
     let random = Math.floor(Math.random()*3);
     if(random == 0) {
@@ -32,13 +33,21 @@ buttons.forEach(function(button){
         const playerChoice = button.dataset.choice;
         const computerChoice = computerPlay();
         const result = playRound(playerChoice, computerChoice);
-        
+
         resultText.textContent = result;
         resultScore.textContent = `${humanScore} - ${computerScore}`;
+        round++;
+        if(round >= 5 || humanScore === 5 || computerScore === 5) { 
+            // End the game
+            round = 0;
+            humanScore = 0;
+            computerScore = 0;
 
+        }
+        roundText.textContent = `Round: ${round}`;
         console.log(playerChoice);
         console.log(computerChoice);
         console.log(result);
-
+        console.log(`Round: ${round}`);
     })
 })
